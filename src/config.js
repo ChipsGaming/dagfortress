de5e9d59acd1ac2caea6b216e4d8fd80fcc3a524
White mage <muzo0@yandex.ru>
2017-11-15 23:58:47 +0300

    fix

--- src/config.js
+++ src/config.js
@@ -1,15 +1,13 @@
 const fs = require('fs');
 
-module.exports = class{
-  build(options, container){
-    return new Promise(function(resolve, reject){
-      fs.readFile(container.get('config_path'), 'utf8', function(err, data){
-        if(err){
-          throw new Error(err);
-        }
-    
-        resolve(JSON.parse(data));
-      }.bind(this));
-    }.bind(this));
+module.exports = {
+  async build(options, container){
+    try {
+      const fsAsync = Util.promisify(fs.readFile);
+      const data = await fsAsync(container.get('config_path'), 'utf8');
+      return JSON.parse(data);
+    } catch(err) {
+      throw err;
+    }
   }
 };
